fc9ec19296a5892e5d4734289f18e3a80e6126c7
Giao Ngo <giaongo.97@gmail.com>
2024-09-27 23:38:02 +0300

    attemp to switch to python-socketio. #24

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,46 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
-  const [socket, setSocket] = useState(null);
+  const socket = io("ws://localhost:5001");
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:5001/ws");
-    setSocket(ws);
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
 
-    ws.onmessage = (event) => {
-      const newMessage = JSON.parse(event.data);
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
-    };
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    socket.on("message", (data) => {
+      console.log(data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
 
     return () => {
-      if (ws.readyState === 1) {
-        ws.close();
-      }
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
     };
   }, []);
 
   const sendMessage = () => {
-    if (input.trim() && socket) {
-      socket.send(JSON.stringify({ message: input }));
-      setInput("");
-    }
+    const message = document.getElementById("message");
+    socket.send(message.value);
   };
 
   return (
     <div>
-      <div>
-        {messages.map((msg, index) => (
-          <div key={index} style={{ color: "white" }}>
-            {msg.message}
-          </div>
-        ))}
-      </div>
       <input
         type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        name="message"
+        id="message"
         onKeyPress={(e) => e.key === "Enter" && sendMessage()}
       />
       <button onClick={sendMessage}>Send</button>
