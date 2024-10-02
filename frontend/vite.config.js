708b2fd1f8a9f2eeaa73153880c92112218878d4
Giao Ngo <giaongo.97@gmail.com>
2024-10-02 19:08:02 +0300

    feat: modify and upload docker container to docker hub. #22

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
   server: {
     host: true,
     port: 5173,
+    strictPort: true,
     watch: {
       usePolling: true,
     },
+    origin: "http://0.0.0.0:5173",
   },
 });
