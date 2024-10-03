4b07b17d8eb17ed638891f8f1b2146c12b26b091
Giao Ngo <giaongo.97@gmail.com>
2024-10-03 20:01:16 +0300

    wip feat: install aws sdk for S3. #23

--- nms/app.js
+++ nms/app.js
@@ -1,4 +1,4 @@
-const NodeMediaServer = require('node-media-server');
+import NodeMediaServer from "node-media-server";
 
 const config = {
   rtmp: {
@@ -6,30 +6,115 @@ const config = {
     chunk_size: 4096,
     gop_cache: false,
     ping: 10,
-    ping_timeout: 20
+    ping_timeout: 20,
   },
   http: {
     port: 8000,
-    allow_origin: '*', 
-    mediaroot: './media', 
-    api: true
-  }, 
+    allow_origin: "*",
+    mediaroot: "./media",
+    api: true,
+  },
   trans: {
-    ffmpeg: '/usr/bin/ffmpeg', 
+    ffmpeg: "/usr/bin/ffmpeg",
     tasks: [
       {
-        app: 'live', 
-        mp4: true, 
-        mp4Flags: '[movflags=flag_keyframe+empty_moov]',
-        hls: true, 
-        hlsFlags: '[hls_time=1:hls_list_size=3:hls_flags=delete_segments+omit_endlist]', 
+        app: "live",
+        mp4: true,
+        mp4Flags: "[movflags=flag_keyframe+empty_moov]",
+        hls: true,
+        hlsFlags:
+          "[hls_time=1:hls_list_size=3:hls_flags=delete_segments+omit_endlist]",
         dash: true,
-        dashFlags: '[f=dash:window_size=2:extra_window_size=4]'
-      }
-    ]
-
-  }
+        dashFlags: "[f=dash:window_size=2:extra_window_size=4]",
+      },
+    ],
+  },
 };
 
-var nms = new NodeMediaServer(config)
+var nms = new NodeMediaServer(config);
 nms.run();
+
+nms.on("preConnect", (id, args) => {
+  console.log(
+    "[NodeEvent on preConnect]",
+    `id=${id} args=${JSON.stringify(args)}`
+  );
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on("postConnect", (id, args) => {
+  console.log(
+    "[NodeEvent on postConnect]",
+    `id=${id} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("doneConnect", (id, args) => {
+  console.log(
+    "[NodeEvent on doneConnect]",
+    `id=${id} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("prePublish", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on prePublish]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on("postPublish", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on postPublish]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("donePublish", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on donePublish]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("prePlay", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on prePlay]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on("postPlay", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on postPlay]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("donePlay", (id, StreamPath, args) => {
+  console.log(
+    "[NodeEvent on donePlay]",
+    `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.on("logMessage", (...args) => {
+  // custom logger log message handler
+});
+
+nms.on("errorMessage", (...args) => {
+  // custom logger error message handler
+});
+
+nms.on("debugMessage", (...args) => {
+  // custom logger debug message handler
+});
+
+nms.on("ffDebugMessage", (...args) => {
+  // custom logger ffmpeg debug message handler
+});
