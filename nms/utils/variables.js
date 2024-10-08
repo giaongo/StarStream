d107b2d1ba4929867aed5ae54bca5671dabb5d45
Giao Ngo <giaongo.97@gmail.com>
2024-10-08 14:16:39 +0300

    wip feat: set up seperate Dockerfile for production. #35

--- nms/utils/variables.js
+++ nms/utils/variables.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://backend:5001";
+const baseUrl = process.env.NODE_ENV === "development" ? "http://backend:5001" : "http://16.16.118.153:5001";
 
 // TODO: Update cdnUrl
 const cdnUrl = "https://d2svo8w7e6o53b.cloudfront.net";
