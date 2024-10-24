3168046edef18aa1fda5ff60165de6591ca5bcc9
Giao Ngo <giaongo.97@gmail.com>
2024-10-24 08:36:56 +0300

    wip: set up fast api. #43

---
+++ videochat/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Query, Path, HTTPException, Depends
+from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated, Literal, Any
+from datetime import datetime
+from fastapi.encoders import jsonable_encoder
+app = FastAPI()
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
