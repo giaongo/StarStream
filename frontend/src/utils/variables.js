f360c5f970de62430f9f855341e053a49a75e635
Giao Ngo <giaongo.97@gmail.com>
2024-11-01 14:40:47 +0200

    wip ops: set up more environment for production. #38

--- frontend/src/utils/variables.js
+++ frontend/src/utils/variables.js
@@ -1,7 +1,5 @@
 const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5001"
-    : "http://localhost/api";
+  process.env.NODE_ENV === "development" ? "http://localhost:5001" : "/api";
 
 const videoChatBaseUrl =
   process.env.NODE_ENV === "development" ? "localhost:5002" : "localhost:5002";
--- nms/utils/variables.js
+++ nms/utils/variables.js
@@ -1,5 +1,12 @@
-const backendUrl = "http://backend:5001";
-const whisperUrl = "http://whisper:9000";
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://backend:5001"
+    : "http://frontend/api";
+
+const whisperUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://whisper:9000"
+    : "http://whisper:9000";
 
 // TODO: Update cdnUrl
 const cdnUrl = "https://d2svo8w7e6o53b.cloudfront.net";
