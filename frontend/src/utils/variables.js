546195e2e0cfeb259eaab280c083d68be752a624
Giao Ngo <giaongo.97@gmail.com>
2024-11-02 13:55:56 +0200

    fix and ops: fix errors in production frontend  Dockfile and docker-compose_web. #38

--- frontend/src/utils/variables.js
+++ frontend/src/utils/variables.js
@@ -2,5 +2,7 @@ const baseUrl =
   process.env.NODE_ENV === "development" ? "http://localhost:5001" : "/api";
 
 const videoChatBaseUrl =
-  process.env.NODE_ENV === "development" ? "localhost:5002" : "localhost:5002";
+  process.env.NODE_ENV === "development"
+    ? "localhost:5002"
+    : "16.170.129.30:5002";
 export { baseUrl, videoChatBaseUrl };
