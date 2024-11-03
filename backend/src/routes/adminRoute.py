ac12ec166b08f2bd6579d0f101a29862258a62fe
Giao Ngo <giaongo.97@gmail.com>
2024-11-03 15:20:11 +0200

    fix: fix bugs in updating streaming url and viewing url. #38

--- backend/src/routes/adminRoute.py
+++ backend/src/routes/adminRoute.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from quart import current_app
+from .. import QuartSIO
 from ..controllers.adminController import add_event_to_db, check_login, delete_event_from_db, get_event_filename_by_id, update_streaming_url, update_viewing_url, validate_image_extension, store_thumbnail
 from quart import Blueprint, request
 from ..models.types import AddEventForm, EventData, LoginForm, StreamingURLUpdateForm, User, ViewingURLUpdateForm
@@ -89,11 +90,11 @@ async def edit_streaming_url():
     """
     form = StreamingURLUpdateForm(await request.form)
     if form.validate():
-        current_app.streaming_url = form.streaming_url.data
+        QuartSIO.get_instance().streaming_url = form.streaming_url.data
         edit_result = await update_streaming_url(form.streaming_url.data)
         if not edit_result:
             return {'msg': 'error updating streaming url'}, 400
-        return {'msg': f'streaming url updated successfully with url {current_app.streaming_url}'}, 200
+        return {'msg': f'streaming url updated successfully with url {QuartSIO.get_instance().streaming_url}'}, 200
     return {'msg': 'invalid streaming url'}, 406
 
 
@@ -104,11 +105,10 @@ async def edit_viewing_url():
     """
     form = ViewingURLUpdateForm(await request.form)
     if form.validate():
-        current_app.viewing_url = form.viewing_url.data
         edit_result = await update_viewing_url(form.viewing_url.data)
         if not edit_result:
             return {'msg': 'error updating viewing url'}, 400
-        return {'msg': f'viewing url updated successfully with url {current_app.viewing_url}'}, 200
+        return {'msg': f'viewing url updated successfully with url {form.viewing_url.data}'}, 200
     return {'msg': 'invalid viewing url'}, 406
 
 
